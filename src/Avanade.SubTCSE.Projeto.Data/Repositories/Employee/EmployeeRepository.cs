8c46f2406a8f492a6ff4ab9a3525c23f1e1b6fbc
Liziane Felix <liziane.felix@hotmail.com>
2021-07-07 08:48:42 -0300

    BaseRepository

--- src/Avanade.SubTCSE.Projeto.Data/Repositories/Employee/EmployeeRepository.cs
+++ src/Avanade.SubTCSE.Projeto.Data/Repositories/Employee/EmployeeRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace Avanade.SubTCSE.Projeto.Data.Repositories.Employee
+﻿using Avanade.SubTCSE.Projeto.Data.Repositories.Base;
+using Avanade.SubTCSE.Projeto.Domain.Aggregates.Employee.Interfaces.Repositories;
+using Avanade.SubTCSE.Projeto.Domain.Base.Repository;
+
+namespace Avanade.SubTCSE.Projeto.Data.Repositories.Employee
 {
-    public class EmployeeRepository
+    public class EmployeeRepository : BaseRepository<Domain.Aggregates.Employee.Entities.Employee, string>
+        , IEmployeeRepository
     {
     }
 }
--- src/Avanade.SubTCSE.Projeto.Data/Repositories/EmployeeRole/EmployeeRoleRepository.cs
+++ src/Avanade.SubTCSE.Projeto.Data/Repositories/EmployeeRole/EmployeeRoleRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace Avanade.SubTCSE.Projeto.Data.Repositories.EmployeeRole
+﻿using Avanade.SubTCSE.Projeto.Data.Repositories.Base;
+using Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Interfaces.Repository;
+
+namespace Avanade.SubTCSE.Projeto.Data.Repositories.EmployeeRole
 {
-    public class EmployeeRoleRepository
+    public class EmployeeRoleRepository : BaseRepository<Domain.Aggregates.EmployeeRole.Entities.EmployeeRole, string>
+        , IEmployeeRoleRepository
     {
+
     }
 }
